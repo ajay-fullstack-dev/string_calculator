a6078da01f63680039371800839f238586c19fce
ajay modi <amajaymodi4@gmail.com>
2024-07-30 18:43:24 +0530

    Basic Add Method

---
+++ spec/lib/string_calculator_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+require_relative '../../app/lib/string_calculator'
+
+RSpec.describe StringCalculator do
+  describe '.add' do
+    it 'returns 0 for an empty string' do
+      expect(StringCalculator.add("")).to eq(0)
+    end
+
+    it 'returns the number itself for a single number' do
+      expect(StringCalculator.add("1")).to eq(1)
+      expect(StringCalculator.add("5")).to eq(5)
+    end
+
+    it 'returns the sum of two numbers' do
+      expect(StringCalculator.add("1,5")).to eq(6)
+      expect(StringCalculator.add("2,3")).to eq(5)
+    end
+  end
+end
